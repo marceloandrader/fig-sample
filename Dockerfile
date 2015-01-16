faaac2571bf25a2cfbcf090649f7b0f268b66c50
Marcelo Andrade <marcelo.andrade.r@gmail.com>
2015-01-16 10:37:53 -0500

    connect to db

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
 FROM php:5.4
+RUN docker-php-ext-install pdo_mysql
 ADD . /code
 WORKDIR /code
--- index.php
+++ index.php
@@ -1,3 +1,7 @@
 <?php
 
+$pdo = new PDO('mysql:host=db', 'root', '');
+$stmt = $pdo->prepare('SELECT NOW()');
+$stmt->execute();
+
 echo 'hello fig';
