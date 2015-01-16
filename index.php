8f8d21ed6bb0bf7cff23779366b75eee5d36669f
Ubuntu <ubuntu@ip-172-31-2-46.ec2.internal>
2015-01-16 15:53:26 +0000

    connecting to the db

--- index.php
+++ index.php
@@ -1,7 +1,15 @@
 <?php
 
-$pdo = new PDO('mysql:host=db', 'root', '');
+echo "<pre>";
+
+$pdo = new PDO('mysql:host=db;port:3306;dbname=mysql;charset=utf8mb4', 'admin', 'plokijuh');
 $stmt = $pdo->prepare('SELECT NOW()');
 $stmt->execute();
 
+while ($row = $stmt->fetch()) {
+    var_dump($row);
+    echo PHP_EOL;
+}
+
+echo PHP_EOL;
 echo 'hello fig';
